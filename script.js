396079317dc10bf5fb2a1c12f4b74f4634448b29
mrioismail <muhamadrioismail@gmail.com>
2023-12-17 13:58:27 +0700

    menambahkan google sheet

--- script.js
+++ script.js
@@ -16,3 +16,19 @@ function reveal() {
       }
    }
 }
+
+// notif google sheet ketika klik kirim
+window.addEventListener('load', function () {
+   const form = document.getElementById('my-form')
+   form.addEventListener('submit', function (e) {
+      e.preventDefault()
+      const data = new FormData(form)
+      const action = e.target.action
+      fetch(action, {
+         method: 'POST',
+         body: data,
+      }).then(() => {
+         alert('Data anda berhasil terkirim bos, terimakasih!')
+      })
+   })
+})
